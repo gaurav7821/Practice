1ad57d9c7b818e4ceb69f1aab829b3fe7e67b93c
chaudharigaurav865@gmail.com <Gaurav@17092002>
2025-06-30 18:17:18 +0530

    Pattern 4

---
+++ Pattern/Pattern4.java
@@ -0,0 +1,18 @@
+package Practice.Pattern;
+
+public class Pattern4 {
+    public static void main(String[] args) {
+        
+        int num=4;
+
+        for(int i=1 ; i<=num ; i++){
+            for(int j=3 ; j>=i ; j--){
+                System.out.print(" ");
+            }
+            for(int k=1 ; k<=i ; k++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
