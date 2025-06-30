4775e43a1de4ee7faa3a6c4b3b5554b8afff1739
chaudharigaurav865@gmail.com <Gaurav@17092002>
2025-06-30 17:52:06 +0530

    Pattern 1

---
+++ Pattern/Pattern1.java
@@ -0,0 +1,15 @@
+
+class Pattern1 {
+    public static void main(String[] args) {
+        int nums=4;
+
+        for(int i=1 ; i<=nums ; i++){
+            
+            for(int j=1 ; j<=i ; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    
+}
