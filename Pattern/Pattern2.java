4fe6e6415ce8f5e1430fed37c7d9be2643210200
chaudharigaurav865@gmail.com <Gaurav@17092002>
2025-06-30 17:55:49 +0530

    Pattern 2

---
+++ Pattern/Pattern2.java
@@ -0,0 +1,15 @@
+package Practice.Pattern;
+
+public class Pattern2 {
+    public static void main(String[] args) {
+        
+        int num=4;
+
+        for(int i=1 ; i<=4 ; i++){
+            for(int j=4 ; j>=i ; j--){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
